5229bf044c70da9bb967af1b41e41d389ae54dc4
Onechock <onechock@gmail.com>
2019-09-08 23:52:07 +0200

    Fixed strange validation problem

--- Backend/Models/Contact.cs
+++ Backend/Models/Contact.cs
@@ -8,6 +8,7 @@ namespace Backend.Models
 {
 	public class Contact
 	{
+		public Contact() { }
 		public Contact(int id, string firstName, string lastName, string email, string phoneNumber, int companyId, string companyName)
 		{
 			Id = id;
